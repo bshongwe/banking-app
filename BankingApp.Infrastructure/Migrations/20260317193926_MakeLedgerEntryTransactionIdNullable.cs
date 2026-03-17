24ebb41703be7475ac8689ab99125249aa5af239
bshongwe <dru.da.beat.hustle@gmail.com>
2026-03-17 22:27:44 +0200

    feat: make TransactionId nullable in LedgerEntry and update migration files

---
+++ BankingApp.Infrastructure/Migrations/20260317193926_MakeLedgerEntryTransactionIdNullable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankingApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeLedgerEntryTransactionIdNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "TransactionId",
+                table: "LedgerEntries",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "TransactionId",
+                table: "LedgerEntries",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
