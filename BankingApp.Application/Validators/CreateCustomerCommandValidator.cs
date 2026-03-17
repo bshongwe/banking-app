8fb96b424743fe5998cf6e29ebc2759fd360277a
bshongwe <dru.da.beat.hustle@gmail.com>
2026-03-17 18:36:18 +0200

    Add validators for CreateAccount, CreateCustomer, and TransferMoney commands

---
+++ BankingApp.Application/Validators/CreateCustomerCommandValidator.cs
@@ -0,0 +1,30 @@
+using BankingApp.Application.CQRS.Commands;
+using FluentValidation;
+
+namespace BankingApp.Application.Validators;
+
+public class CreateCustomerCommandValidator : AbstractValidator<CreateCustomerCommand>
+{
+    public CreateCustomerCommandValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty()
+            .WithMessage("First name is required.")
+            .MaximumLength(100)
+            .WithMessage("First name cannot exceed 100 characters.");
+
+        RuleFor(x => x.LastName)
+            .NotEmpty()
+            .WithMessage("Last name is required.")
+            .MaximumLength(100)
+            .WithMessage("Last name cannot exceed 100 characters.");
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .WithMessage("Email is required.")
+            .EmailAddress()
+            .WithMessage("Email must be a valid email address.")
+            .MaximumLength(200)
+            .WithMessage("Email cannot exceed 200 characters.");
+    }
+}
